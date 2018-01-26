229674db5a0c2100d84da9c645c46e052eeecbd7
shima <shima.khosravei@gmail.com>
2018-01-26 21:41:59 +0330

    img and p styles

--- shimons/Views/dashbord_views.py
+++ shimons/Views/dashbord_views.py
@@ -7,8 +7,15 @@ from shimons.Views.view_misc import get_chart_data_from_folder, save_file, \
     proccess_analysis, proccess_search_data
 from shimons.models import DashboardPost, Request, DetectionAlgorithm, RequestAttachPattern, \
     RequestSelectPattern
-from Documentations import Documentation
-
+# from Documentations import Documentation
+Documentation = {
+    "Overview": '<p>PDB<sub>GTGT</sub> is based on the automatic generation of testbeds using graph theory.'
+                ' Figure 1 demonstrates the PDB<sub>GTGT</sub> use case diagram.</p>'
+                '<p><img src="/static/sqlab/img/Figure1_PDBgtgt_use_case.jpg" alt="Figure 1: PDB_GTGT  use case diagram"/></p>'
+                '<p>In the following, to address more details about PDB<sub>GTGT</sub>, '
+                'we will introduce the process of evaluating pattern detection methods in our proposed benchmark.</p>'
+                '<p><img src="/static/sqlab/img/Figure2_evaluation_PDBgtgt.png" alt="Figure 2: The process of evaluation in PDB_GTGT"/></p>'
+}
 
 @login_required()
 def dashboard(request):
